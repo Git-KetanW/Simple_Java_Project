108cccdf3c4e5ec5134fd942fce959e05d4ef5eb
Ketan Walde <ketan.walde.1@gmail.com>
2024-03-22 15:51:09 +0530

    uploading version 1 of java project

---
+++ src/project1/SimpleProj.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class SimpleProj {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Ketan";
+		String str2 = "Rana";
+		int a = 28;
+		
+		System.out.println("My name is :"+str1+" "+str2+" and I'm "+a+" year old now!");
+
+	}
+
+}
